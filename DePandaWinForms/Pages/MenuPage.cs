f7b8048804e53974392a5a386b6fcc2677395f88
smoerman99 <smoerman99@gmail.com>
2020-04-07 12:21:28 +0200

    Menu kan menu items toevoegen

--- DePandaWinForms/Pages/MenuPage.cs
+++ DePandaWinForms/Pages/MenuPage.cs
@@ -8,22 +8,34 @@ using DePandaLib.DAL;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using DePandaLib.Entities;
+//using DePandaLib.Entities;
+
+using System.Globalization;
+
 
 namespace DePandaWinForms.Pages
 {
     public partial class MenuPage : Form
     {
+
+        DataStorageHandler dataStorageHandler = new DataStorageHandler();
+
+
         public MenuPage()
         {
             InitializeComponent();
+            LoadInMenuItems();
+        }
 
-            List<Dish> dishes = new List<Dish>();
+        public void LoadInMenuItems()
+        {
+            var MenuItems = DataStorageHandler.Storage.StockDishes;
 
-            //DataStorageHandler.Storage.StockDishes;
-
-
-
+            foreach (var menuItem in MenuItems)
+            {
+                MenuItemsList.Items.Add(menuItem.Name);
+                Console.WriteLine(menuItem.Name);
+            }
         }
 
         private void Pincode_Click(object sender, EventArgs e)
@@ -34,7 +46,18 @@ namespace DePandaWinForms.Pages
         {
         }
 
+        private void AddMenuItemButton_Click(object sender, EventArgs e)
+        {
+            CultureInfo cultures = new CultureInfo("en-US");
 
+            decimal Price = Convert.ToDecimal(PriceInput.Text, cultures);
 
+            DataStorageHandler.Storage.StockDishes.Add(new DePandaLib.Entities.Dish() { Name = NameInput.Text, Price = Price });
+
+            MenuItemsList.Items.Clear();
+
+            LoadInMenuItems();
+            DataStorageHandler.SaveChanges();
+        }
     }
 }
