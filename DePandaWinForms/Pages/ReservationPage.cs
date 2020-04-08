81f38d0d40bd849c10cd9fb6a3a668336e672aa0
Meesw1 <60971894+Meesw1@users.noreply.github.com>
2020-04-08 12:22:59 +0200

    reservering begin design gemaakt

--- DePandaWinForms/Pages/ReservationPage.cs
+++ DePandaWinForms/Pages/ReservationPage.cs
@@ -16,5 +16,40 @@ namespace DePandaWinForms.Pages
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Naam1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Hoeveelpersonen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Tijd1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ReservationPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
     }
 }
