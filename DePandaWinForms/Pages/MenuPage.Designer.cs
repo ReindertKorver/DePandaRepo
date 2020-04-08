6b9a18d37246494adf2aca6cee5165d1bdeadd71
Jelmar Dekker <jelmardekker@gmail.com>
2020-04-08 13:58:34 +0200

    vooral design opzet

--- DePandaWinForms/Pages/MenuPage.Designer.cs
+++ DePandaWinForms/Pages/MenuPage.Designer.cs
@@ -31,7 +31,28 @@
             this.Pincode = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.MenuGrid = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.Searchbox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.MenuGrid)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Pincode
@@ -40,9 +61,9 @@
             this.Pincode.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Pincode.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(45)))), ((int)(((byte)(51)))));
             this.Pincode.Location = new System.Drawing.Point(0, 0);
-            this.Pincode.Margin = new System.Windows.Forms.Padding(3, 25, 3, 0);
+            this.Pincode.Margin = new System.Windows.Forms.Padding(6, 48, 6, 0);
             this.Pincode.Name = "Pincode";
-            this.Pincode.Size = new System.Drawing.Size(800, 37);
+            this.Pincode.Size = new System.Drawing.Size(1600, 71);
             this.Pincode.TabIndex = 13;
             this.Pincode.Text = "Menu";
             this.Pincode.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -52,9 +73,10 @@
             // 
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(627, 11);
+            this.textBox1.Location = new System.Drawing.Point(1254, 21);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(150, 29);
+            this.textBox1.Size = new System.Drawing.Size(298, 50);
             this.textBox1.TabIndex = 14;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -64,23 +86,224 @@
             this.MenuGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.MenuGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.MenuGrid.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.MenuGrid.Location = new System.Drawing.Point(0, 46);
+            this.MenuGrid.Location = new System.Drawing.Point(0, 88);
+            this.MenuGrid.Margin = new System.Windows.Forms.Padding(6);
             this.MenuGrid.Name = "MenuGrid";
-            this.MenuGrid.Size = new System.Drawing.Size(800, 404);
+            this.MenuGrid.RowHeadersWidth = 82;
+            this.MenuGrid.Size = new System.Drawing.Size(1600, 777);
             this.MenuGrid.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(312, 67);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Fou yong hai";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(312, 67);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Bami";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 149);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(312, 67);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Taufu";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 222);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(312, 70);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Water";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(260, 762);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(222, 63);
+            this.button5.TabIndex = 21;
+            this.button5.Text = "Nieuw item";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Controls.Add(this.button8);
+            this.flowLayoutPanel1.Controls.Add(this.button9);
+            this.flowLayoutPanel1.Controls.Add(this.button10);
+            this.flowLayoutPanel1.Controls.Add(this.button11);
+            this.flowLayoutPanel1.Controls.Add(this.button12);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(57, 210);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(425, 534);
+            this.flowLayoutPanel1.TabIndex = 22;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(3, 298);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(312, 70);
+            this.button8.TabIndex = 20;
+            this.button8.Text = "Fanta";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(3, 374);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(312, 70);
+            this.button9.TabIndex = 21;
+            this.button9.Text = "...";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(3, 450);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(312, 70);
+            this.button10.TabIndex = 22;
+            this.button10.Text = "...";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(3, 526);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(312, 70);
+            this.button11.TabIndex = 23;
+            this.button11.Text = "...";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(3, 602);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(312, 70);
+            this.button12.TabIndex = 24;
+            this.button12.Text = "...";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // Searchbox
+            // 
+            this.Searchbox.Location = new System.Drawing.Point(57, 173);
+            this.Searchbox.Name = "Searchbox";
+            this.Searchbox.Size = new System.Drawing.Size(329, 31);
+            this.Searchbox.TabIndex = 23;
+            this.Searchbox.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button13);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Location = new System.Drawing.Point(581, 153);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(940, 591);
+            this.panel1.TabIndex = 24;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Teal;
+            this.label3.Location = new System.Drawing.Point(33, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 33);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Notities:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Teal;
+            this.label2.Location = new System.Drawing.Point(33, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 37);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Prijs:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Teal;
+            this.label1.Location = new System.Drawing.Point(23, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(395, 55);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nieuw menu item";
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(56, 539);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(47, 38);
+            this.button13.TabIndex = 2;
+            this.button13.Text = "button13";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(3, 539);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(47, 38);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(791, 520);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(131, 57);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Opslaan";
+            this.button6.UseVisualStyleBackColor = true;
             // 
             // MenuPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Searchbox);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.MenuGrid);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.Pincode);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "MenuPage";
             this.Text = "Reservations";
             ((System.ComponentModel.ISupportInitialize)(this.MenuGrid)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,5 +314,24 @@
         private System.Windows.Forms.Label Pincode;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataGridView MenuGrid;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.TextBox Searchbox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
     }
 }
--- DePandaWinForms/Pages/MenuPage.cs
+++ DePandaWinForms/Pages/MenuPage.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DePandaLib.DAL;
 
 namespace DePandaWinForms.Pages
 {
@@ -24,5 +25,31 @@ namespace DePandaWinForms.Pages
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        List<string> listcollection = new List<string>();
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) == false)
+            {
+                foreach (string str in listcollection)
+                {
+                    if (str.StartsWith(textBox1.Text))
+                    {
+                        //flowLayoutPanel1.
+                    }
+                }
+            }
+        }
+
     }
 }
