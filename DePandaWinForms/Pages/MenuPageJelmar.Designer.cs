816aa4e20803905d4729f287236c22291052c8f9
Reindert Korver <ReindertKorver@gmail.com>
2020-04-09 15:05:19 +0200

    Jelmars menu toegevoegd

---
+++ DePandaWinForms/Pages/MenuPageJelmar.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace DePandaWinForms.Pages
+{
+    partial class MenuPageJelmar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Pincode = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.MenuGrid = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.NieuwItemButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.Searchbox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.NotitiesInput = new System.Windows.Forms.TextBox();
+            this.PrijsInput = new System.Windows.Forms.TextBox();
+            this.NieuwMenuItemTekstbox = new System.Windows.Forms.TextBox();
+            this.NotitiesLabel = new System.Windows.Forms.Label();
+            this.PrijsLabel = new System.Windows.Forms.Label();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.OpslaanButton = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.MenuGrid)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Pincode
+            // 
+            this.Pincode.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Pincode.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pincode.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(45)))), ((int)(((byte)(51)))));
+            this.Pincode.Location = new System.Drawing.Point(0, 0);
+            this.Pincode.Margin = new System.Windows.Forms.Padding(6, 48, 6, 0);
+            this.Pincode.Name = "Pincode";
+            this.Pincode.Size = new System.Drawing.Size(1600, 71);
+            this.Pincode.TabIndex = 13;
+            this.Pincode.Text = "Menu";
+            this.Pincode.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(1254, 21);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(298, 50);
+            this.textBox1.TabIndex = 14;
+            // 
+            // MenuGrid
+            // 
+            this.MenuGrid.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.MenuGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MenuGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MenuGrid.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.MenuGrid.Location = new System.Drawing.Point(0, 88);
+            this.MenuGrid.Margin = new System.Windows.Forms.Padding(6);
+            this.MenuGrid.Name = "MenuGrid";
+            this.MenuGrid.RowHeadersWidth = 82;
+            this.MenuGrid.Size = new System.Drawing.Size(1600, 777);
+            this.MenuGrid.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(312, 67);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Fou yong hai";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(312, 67);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Bami";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 149);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(312, 67);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Taufu";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 222);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(312, 70);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Water";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // NieuwItemButton
+            // 
+            this.NieuwItemButton.Location = new System.Drawing.Point(260, 762);
+            this.NieuwItemButton.Name = "NieuwItemButton";
+            this.NieuwItemButton.Size = new System.Drawing.Size(222, 63);
+            this.NieuwItemButton.TabIndex = 21;
+            this.NieuwItemButton.Text = "Nieuw item";
+            this.NieuwItemButton.UseVisualStyleBackColor = true;
+            this.NieuwItemButton.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Controls.Add(this.button8);
+            this.flowLayoutPanel1.Controls.Add(this.button9);
+            this.flowLayoutPanel1.Controls.Add(this.button10);
+            this.flowLayoutPanel1.Controls.Add(this.button11);
+            this.flowLayoutPanel1.Controls.Add(this.button12);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(57, 210);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(425, 534);
+            this.flowLayoutPanel1.TabIndex = 22;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(3, 298);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(312, 70);
+            this.button8.TabIndex = 20;
+            this.button8.Text = "Fanta";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(3, 374);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(312, 70);
+            this.button9.TabIndex = 21;
+            this.button9.Text = "...";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(3, 450);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(312, 70);
+            this.button10.TabIndex = 22;
+            this.button10.Text = "...";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(3, 526);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(312, 70);
+            this.button11.TabIndex = 23;
+            this.button11.Text = "...";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(3, 602);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(312, 70);
+            this.button12.TabIndex = 24;
+            this.button12.Text = "...";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // Searchbox
+            // 
+            this.Searchbox.Location = new System.Drawing.Point(57, 173);
+            this.Searchbox.Name = "Searchbox";
+            this.Searchbox.Size = new System.Drawing.Size(329, 31);
+            this.Searchbox.TabIndex = 23;
+            this.Searchbox.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.NotitiesInput);
+            this.panel1.Controls.Add(this.PrijsInput);
+            this.panel1.Controls.Add(this.NieuwMenuItemTekstbox);
+            this.panel1.Controls.Add(this.NotitiesLabel);
+            this.panel1.Controls.Add(this.PrijsLabel);
+            this.panel1.Controls.Add(this.button13);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.OpslaanButton);
+            this.panel1.Location = new System.Drawing.Point(581, 153);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(940, 591);
+            this.panel1.TabIndex = 24;
+            this.panel1.Visible = false;
+            // 
+            // NotitiesInput
+            // 
+            this.NotitiesInput.BackColor = System.Drawing.Color.DarkGray;
+            this.NotitiesInput.ForeColor = System.Drawing.Color.Teal;
+            this.NotitiesInput.Location = new System.Drawing.Point(313, 151);
+            this.NotitiesInput.Multiline = true;
+            this.NotitiesInput.Name = "NotitiesInput";
+            this.NotitiesInput.Size = new System.Drawing.Size(609, 274);
+            this.NotitiesInput.TabIndex = 8;
+            // 
+            // PrijsInput
+            // 
+            this.PrijsInput.BackColor = System.Drawing.Color.DarkGray;
+            this.PrijsInput.ForeColor = System.Drawing.Color.Teal;
+            this.PrijsInput.Location = new System.Drawing.Point(313, 101);
+            this.PrijsInput.Multiline = true;
+            this.PrijsInput.Name = "PrijsInput";
+            this.PrijsInput.Size = new System.Drawing.Size(117, 37);
+            this.PrijsInput.TabIndex = 7;
+            // 
+            // NieuwMenuItemTekstbox
+            // 
+            this.NieuwMenuItemTekstbox.BackColor = System.Drawing.Color.DarkGray;
+            this.NieuwMenuItemTekstbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NieuwMenuItemTekstbox.ForeColor = System.Drawing.Color.Teal;
+            this.NieuwMenuItemTekstbox.Location = new System.Drawing.Point(30, 20);
+            this.NieuwMenuItemTekstbox.Multiline = true;
+            this.NieuwMenuItemTekstbox.Name = "NieuwMenuItemTekstbox";
+            this.NieuwMenuItemTekstbox.Size = new System.Drawing.Size(383, 61);
+            this.NieuwMenuItemTekstbox.TabIndex = 6;
+            this.NieuwMenuItemTekstbox.Text = "Nieuw menu item";
+            this.NieuwMenuItemTekstbox.TextChanged += new System.EventHandler(this.textBox2_TextChanged_1);
+            // 
+            // NotitiesLabel
+            // 
+            this.NotitiesLabel.AutoSize = true;
+            this.NotitiesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NotitiesLabel.ForeColor = System.Drawing.Color.Teal;
+            this.NotitiesLabel.Location = new System.Drawing.Point(33, 154);
+            this.NotitiesLabel.Name = "NotitiesLabel";
+            this.NotitiesLabel.Size = new System.Drawing.Size(121, 33);
+            this.NotitiesLabel.TabIndex = 5;
+            this.NotitiesLabel.Text = "Notities:";
+            // 
+            // PrijsLabel
+            // 
+            this.PrijsLabel.AutoSize = true;
+            this.PrijsLabel.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.PrijsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PrijsLabel.ForeColor = System.Drawing.Color.Teal;
+            this.PrijsLabel.Location = new System.Drawing.Point(33, 101);
+            this.PrijsLabel.Name = "PrijsLabel";
+            this.PrijsLabel.Size = new System.Drawing.Size(88, 37);
+            this.PrijsLabel.TabIndex = 4;
+            this.PrijsLabel.Text = "Prijs:";
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(56, 539);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(47, 38);
+            this.button13.TabIndex = 2;
+            this.button13.Text = "button13";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(3, 539);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(47, 38);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // OpslaanButton
+            // 
+            this.OpslaanButton.Location = new System.Drawing.Point(791, 520);
+            this.OpslaanButton.Name = "OpslaanButton";
+            this.OpslaanButton.Size = new System.Drawing.Size(131, 57);
+            this.OpslaanButton.TabIndex = 0;
+            this.OpslaanButton.Text = "Opslaan";
+            this.OpslaanButton.UseVisualStyleBackColor = true;
+            this.OpslaanButton.Click += new System.EventHandler(this.OpslaanButton_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // MenuPageJelmar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Searchbox);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.NieuwItemButton);
+            this.Controls.Add(this.MenuGrid);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Pincode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "MenuPageJelmar";
+            this.Text = "Reservations";
+            ((System.ComponentModel.ISupportInitialize)(this.MenuGrid)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Pincode;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView MenuGrid;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.TextBox Searchbox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label NotitiesLabel;
+        private System.Windows.Forms.Label PrijsLabel;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button OpslaanButton;
+        private System.Windows.Forms.Button NieuwItemButton;
+        private System.Windows.Forms.TextBox NieuwMenuItemTekstbox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox NotitiesInput;
+        private System.Windows.Forms.TextBox PrijsInput;
+    }
+}
---
+++ DePandaWinForms/Pages/MenuPageJelmar.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DePandaLib.DAL;
+using DePandaLib.Entities;
+
+namespace DePandaWinForms.Pages
+{
+    public partial class MenuPageJelmar : Form
+    {
+        public MenuPageJelmar()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+        }
+
+        List<string> listcollection = new List<string>();
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) == false)
+            {
+                foreach (string str in listcollection)
+                {
+                    if (str.StartsWith(textBox1.Text))
+                    {
+                        //flowLayoutPanel1.
+                    }
+                }
+            }
+        }
+        
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+            if (NieuwMenuItemTekstbox.Text == "Nieuw menu item") 
+            {
+                NieuwMenuItemTekstbox.Text = "";
+                NieuwMenuItemTekstbox.ForeColor = Color.Black;
+            }
+        }
+
+        private void OpslaanButton_Click(object sender, EventArgs e)
+        {
+            Dish dish = new Dish();
+            dish.Name = NieuwMenuItemTekstbox.Text;
+            dish.Price = Convert.ToDecimal(PrijsInput.Text);
+            DataStorageHandler.Storage.StockDishes.Add(dish);
+            panel1.Visible = false;
+        }
+    }
+}
