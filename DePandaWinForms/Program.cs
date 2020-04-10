fe3d00c209ee7b49081303bb9fdab2dd90f43d58
Reindert Korver <ReindertKorver@gmail.com>
2020-04-10 12:19:48 +0200

    Exception Handler

--- DePandaWinForms/Program.cs
+++ DePandaWinForms/Program.cs
@@ -4,6 +4,7 @@ using DePandaWinForms.Pages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,15 +20,48 @@ namespace DePandaWinForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            // Set the unhandled exception mode to force all Windows Forms errors
+            // to go through our handler.
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+
+            // Add the event handler for handling non-UI thread exceptions to the event.
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+            Application.ThreadException += Application_ThreadException;
             DataStorageHandler.StorageFileLocation = "DataStorage.json";
             DataStorageHandler.Init();
 
             //DataStorageHandler.Storage.StockDishes.Add();
-            
+
             Application.Run(new Form1(FormWindowState.Normal));
             // change Form to Login Application.Run(new Login(false));
-          
+
             DataStorageHandler.SaveChanges();
+        }
+
+        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            ShowExceptionMessage(e.Exception);
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            ShowExceptionMessage((e.ExceptionObject as Exception));
+        }
+
+        private static void ShowExceptionMessage(Exception e)
+        {
+            bool savedData = false;
+            try
+            {
+                DataStorageHandler.SaveChanges();
+                savedData = true;
+            }
+            catch (Exception ex)
+            {
+                savedData = false;
+            }
+            MessageBox.Show("Fout:\n" + e.Message + "\n" + (savedData ? "\nGecachete data is opgeslagen." : "\nGecachete data kon niet worden opgeslagen, dit betekent dat er kans is op verlies van data."), "Er is een uitzondering opgetreden", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
