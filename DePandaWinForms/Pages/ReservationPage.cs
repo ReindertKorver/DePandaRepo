c5ebd89ed2678a9285eb091669881233830d8195
BartWesthoff-HRO <60735088+BartWesthoff-HRO@users.noreply.github.com>
2020-05-06 10:26:27 +0200

    alternative dates werkt nu bij het aanmaken van een reservering

--- DePandaWinForms/Pages/ReservationPage.cs
+++ DePandaWinForms/Pages/ReservationPage.cs
@@ -37,10 +37,25 @@ namespace DePandaWinForms.Pages
                 listView.Items.Add(item);
             }
         }
-        
+
+        List<DePandaClassLib.Entities.AlternativeDate> ListOfAlternativeDates = DataStorageHandler.Storage.Settings.AlternativeDates;
         public bool ReservationTimeCheck()
         {
             DayOfWeek[] days = { DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday, DayOfWeek.Saturday, DayOfWeek.Sunday };
+            for (int i = 0; i < ListOfAlternativeDates.Count; i++)
+            {
+                if (dateTimePicker1.Value.Date == ListOfAlternativeDates[i].StartDate.Date)
+                {
+                    var time = dateTimePicker2.Value.TimeOfDay;
+                    var time2 = dateTimePicker3.Value.TimeOfDay;
+                    if (time < ListOfAlternativeDates[i].StartDate.TimeOfDay || time2 > ListOfAlternativeDates[i].EndDate.TimeOfDay)
+                    {
+                        return false;
+
+                    }
+                    return true;
+                }
+            }
             for (int i = 0; i < days.Length; i++)
             {
                 if (dateTimePicker1.Value.DayOfWeek == days[i])
@@ -54,8 +69,11 @@ namespace DePandaWinForms.Pages
                     return true;
                 }
             }
+     
+            
             return true;
         }
+       
 
         // add reservation
         private void button1_Click(object sender, EventArgs e)
@@ -86,7 +104,7 @@ namespace DePandaWinForms.Pages
                 return;
             }
 
-            DataStorageHandler.Storage.Reservations.Add(new DePandaLib.Entities.Reservation(){ OnTheNameOf = txtNaam.Text, Date = dateTime, Time = timeSpan, AmountOfPeople = decimal.ToInt32(PersonenBox.Value), Table = tafelnr, Specifications = txtBijzonder.Text });
+            DataStorageHandler.Storage.Reservations.Add(new DePandaLib.Entities.Reservation() { OnTheNameOf = txtNaam.Text, Date = dateTime, Time = timeSpan, AmountOfPeople = decimal.ToInt32(PersonenBox.Value), Table = tafelnr, Specifications = txtBijzonder.Text });
             listView.Items.Clear();
             LoadExistingReservations();
 
@@ -94,7 +112,7 @@ namespace DePandaWinForms.Pages
             PersonenBox.Value = 1;
 
             txtNaam.Clear(); txtBijzonder.Clear();
-            
+
         }
 
         // remove reservation
@@ -106,7 +124,7 @@ namespace DePandaWinForms.Pages
             if (listView.SelectedItems.Count > 0)
             {
                 int v = int.Parse(Reservations[listView.SelectedItems[0].Index].Table);
-                panels[v-1].BackColor = Color.Gainsboro;
+                panels[v - 1].BackColor = Color.Gainsboro;
 
                 Reservations.Remove(Reservations[listView.SelectedItems[0].Index]);
                 listView.Items.Remove(listView.SelectedItems[0]);
@@ -145,9 +163,9 @@ namespace DePandaWinForms.Pages
         private void Cleartables()
         {
             Panel[] panels = new Panel[] { panel1, panel2, panel3, panel4, panel5, panel6, panel7, panel8, panel9, panel10, panel11, panel12 };
-            for(int i = 0; i<panels.Length; i++)
+            for (int i = 0; i < panels.Length; i++)
             {
-                if(panels[i].BackColor == Color.Green)
+                if (panels[i].BackColor == Color.Green)
                 {
                     panels[i].BackColor = Color.Gray;
                 }
@@ -161,7 +179,7 @@ namespace DePandaWinForms.Pages
             Panel[] panels = new Panel[] { panel1, panel2, panel3, panel4, panel5, panel6, panel7, panel8, panel9, panel10, panel11, panel12 };
 
 
-            foreach(var panel in panels)
+            foreach (var panel in panels)
             {
                 if (CurX > panel.Location.X && CurX < panel.Location.X + panel.Width && CurY > panel.Location.Y && CurY < panel.Location.Y + panel.Height)
                 {
@@ -172,7 +190,7 @@ namespace DePandaWinForms.Pages
                     if (panel.BackColor == Color.Gainsboro)
                     {
                         int TableSelected = 0;
-                        for(int i = 0; i < panels.Length; i++)
+                        for (int i = 0; i < panels.Length; i++)
                         {
                             if (panels[i].BackColor == Color.Green)
                             {
@@ -192,7 +210,7 @@ namespace DePandaWinForms.Pages
 
                 }
             }
-            
+
         }
         private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
         {
@@ -210,8 +228,8 @@ namespace DePandaWinForms.Pages
             {
                 DateTime dateTime = reservation.Date;
                 DateTime dateTime2 = reservation.Date + reservation.Time;
-                Panel panel = panels[int.Parse(reservation.Table)-1];
-                
+                Panel panel = panels[int.Parse(reservation.Table) - 1];
+
                 if (seen.Contains(panel))
                 {
                     continue;
@@ -219,8 +237,8 @@ namespace DePandaWinForms.Pages
 
                 if (dateTime.Date == dateTimePicker1.Value.Date)
                 {
-                    if (dateTimePicker2.Value.TimeOfDay >= dateTime.TimeOfDay && dateTimePicker2.Value.AddMinutes(1).TimeOfDay < dateTime2.TimeOfDay || 
-                        dateTimePicker3.Value.TimeOfDay > dateTime.TimeOfDay && dateTimePicker3.Value.TimeOfDay <= dateTime2.TimeOfDay || 
+                    if (dateTimePicker2.Value.TimeOfDay >= dateTime.TimeOfDay && dateTimePicker2.Value.AddMinutes(1).TimeOfDay < dateTime2.TimeOfDay ||
+                        dateTimePicker3.Value.TimeOfDay > dateTime.TimeOfDay && dateTimePicker3.Value.TimeOfDay <= dateTime2.TimeOfDay ||
                         dateTimePicker2.Value.TimeOfDay < dateTime.TimeOfDay && dateTimePicker3.Value.TimeOfDay > dateTime2.TimeOfDay)
                     {
                         panel.BackColor = Color.Gray;
@@ -234,7 +252,7 @@ namespace DePandaWinForms.Pages
                 else
                 {
                     panel.BackColor = Color.Gainsboro;
-                    
+
                 }
             }
         }
